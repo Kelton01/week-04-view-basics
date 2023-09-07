e330e6d0e654e22c46f5623ca3b06425eeb8d99e
Kelton Benson <kelton01@csu.fullerton.edu>
2023-09-07 16:43:01 -0700

    first commit

---
+++ week-04-view-basics/week-04-view-basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-04-view-basics
+//
+//  Created by kelton on 9/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ week-04-view-basics/week-04-view-basicsUITests/week_04_view_basicsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  week_04_view_basicsUITestsLaunchTests.swift
+//  week-04-view-basicsUITests
+//
+//  Created by kelton on 9/7/23.
+//
+
+import XCTest
+
+final class week_04_view_basicsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
